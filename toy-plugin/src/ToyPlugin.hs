f6837f321aa7881e453cde0038c176224e1ea3b4
Alfredo Di Napoli <alfredo.dinapoli@gmail.com>
2020-09-09 14:47:33 +0200

    Reproduce the problem

--- toy-plugin/src/ToyPlugin.hs
+++ toy-plugin/src/ToyPlugin.hs
@@ -2,13 +2,14 @@
 module ToyPlugin where
 
 import qualified Outputable as O
+import HscMain
+import TcRnMonad
 import GHC
 import Control.Monad
 import CoreMonad
 import DynFlags
 import HscTypes
 import GHC.Paths (libdir)
-import TcRnTypes
 import Plugins
 
 
@@ -18,25 +19,33 @@ plugin = defaultPlugin { pluginRecompile = \_ -> pure ForceRecompile
                        , installCoreToDos   = coreHook
                        }
 
+-- | Set the optimization level to 0 (\"-O0\") and clean any plugin to avoid that calling 'desugarModule'
+-- might accidentally loop everything.
 cleanSummary :: ModSummary -> ModSummary
 cleanSummary modSummary = modSummary
-  { ms_hspp_opts = updOptLevel 2 (ms_hspp_opts modSummary) { cachedPlugins = [], staticPlugins = [] }
+  { ms_hspp_opts = unoptFlags (ms_hspp_opts modSummary) { cachedPlugins = [], staticPlugins = [] }
   }
+
+unoptFlags :: DynFlags -> DynFlags
+unoptFlags = updOptLevel 0
+
+cleanEnv :: HscEnv -> HscEnv
+cleanEnv env = env { hsc_dflags = unoptFlags (hsc_dflags env) }
 
 typecheckHook :: [CommandLineOption] -> ModSummary -> TcGblEnv -> TcM TcGblEnv
 typecheckHook _ modSummary gblEnv = do
+  -- Try to produce core binds with optimisations turned off by reusing the 'TcGblEnv'
+  -- we got as input.
   let modSum' = cleanSummary modSummary
+  env <- cleanEnv <$> getTopEnv
   coreBinds <- runGhcT (Just libdir) $ do
-    parsed      <- parseModule     modSum'
-    typechecked <- typecheckModule parsed
-    mg_binds . dm_core_module <$> desugarModule typechecked
+    mg_binds <$> liftIO (hscDesugar env modSum' gblEnv)
   liftIO $ do
     putStrLn $ (replicate 80 '*')
     putStrLn "Core Binds generated with -O0:"
     putStrLn $ O.showSDocUnsafe (O.vcat $ map O.ppr coreBinds)
     putStrLn $ (replicate 80 '*')
   pure gblEnv
-
 
 
 coreHook :: [CommandLineOption] -> [CoreToDo] -> CoreM [CoreToDo]
@@ -53,6 +62,5 @@ coreHook _ passes = pure (CoreDoPluginPass "ToyPluginPass" myPass : passes)
           putStrLn $ (O.showSDocUnsafe $ O.vcat $ map O.ppr (mg_binds guts))
           putStrLn $ (replicate 80 '*')
       pure guts
-
-getOptLevel :: (Functor m, HasDynFlags m) => m Int
-getOptLevel = optLevel <$> getDynFlags
+    getOptLevel :: (Functor m, HasDynFlags m) => m Int
+    getOptLevel = optLevel <$> getDynFlags
